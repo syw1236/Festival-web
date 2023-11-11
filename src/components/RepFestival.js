e077a1e083f7b607c08cc04cb651ae8981b20093
YeWon Seo <syw1236@naver.com>
2023-11-12 05:48:02 +0900

    Create RepFestival.js
    RepFestival.js 생성 커밋

---
+++ src/components/RepFestival.js
@@ -0,0 +1,37 @@
+import "./RepFestival.css";
+import LookFestivalDetail from "./LookFestivalDetail";
+//import FestivalSimple from "./FistivalSimple";
+function RepFestival({ data }) {
+  //const array = data;
+  const today = new Date();
+  var dateArray = data.date[1].split("-");
+  var enddayArray = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
+  // const day = enddayArray - today;
+  // //const day = data.date[1].toString() - today;
+  // //console.log(dateArray[0], dateArray[1], dateArray[2]);
+  // console.log(dateArray);
+  // const daysRemaining = Math.ceil(day / (1000 * 60 * 60 * 24));
+
+  const timeDiff = enddayArray.getTime() - today.getTime();
+  const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+  return (
+    <div className="RepFestivalDiv">
+      <ul>
+        <li>
+          <span className="dayRemain">D-{daysLeft}</span>
+        </li>
+        <li>
+          <span className="name">{data.name}</span>
+        </li>
+        <li>
+          <span className="description">{data.description}</span>
+        </li>
+        <li>
+          <LookFestivalDetail />
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default RepFestival;
