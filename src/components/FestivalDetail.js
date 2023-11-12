8ce2eac33ae60084f5e40a412a177dc2a749173a
juwonhee <jwonhee398@gmail.com>
2023-11-13 02:36:50 +0900

    SearchList first commit

---
+++ src/components/FestivalDetail.js
@@ -0,0 +1,12 @@
+// FestivalDetail.js
+import React from 'react';
+
+const FestivalDetail = () => {
+  return (
+    <div>
+      <h1>데이터 안녕</h1>
+    </div>
+  );
+};
+
+export default FestivalDetail;
