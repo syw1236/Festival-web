ec460b338cd3cfb0cd3c5f47d3f2b5c0aefd35ea
YeWon Seo <syw1236@naver.com>
2023-11-12 15:11:06 +0900

    Update FistivalSimple.js

--- src/components/FistivalSimple.js
+++ src/components/FistivalSimple.js
@@ -1,4 +1,17 @@
-function FestivalSimple({ data }) {
-  return;
+import "./FestivalSimple.css";
+function FestivalSimple({ festival }) {
+  return (
+    <div className="festivalContain">
+      <img
+        className="festivalImage"
+        width={250}
+        height={250}
+        src={festival.poster}
+        alt="festival.poster"
+      />
+      <div className="festivalName">{festival.name}</div>
+      <div className="festivalLocation">{festival.detail_locations}</div>
+    </div>
+  );
 }
 export default FestivalSimple;
