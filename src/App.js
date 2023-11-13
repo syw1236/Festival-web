a3e2da9fb8bf857c64bd7208b3c82dbb42a4c195
YeWon Seo <syw1236@naver.com>
2023-11-14 01:17:33 +0900

    Remove festivalsData.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import CountryDetail from "./components/CountryDetail";
-import festivalsData from "./data/festivalsData";
+
 function App() {
   return (
     <div>
-      <CountryDetail data={festivalsData} />
+      <CountryDetail />
     </div>
   );
 }
