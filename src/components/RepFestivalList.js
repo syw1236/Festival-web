55fde92bb35c095258e2df90c11d2a7490bfaa23
YeWon Seo <syw1236@naver.com>
2023-11-19 19:08:21 +0900

    Update RepFestivalList.js

--- src/components/RepFestivalList.js
+++ src/components/RepFestivalList.js
@@ -1,10 +1,11 @@
 import RepFestival from "./RepFestival";
-
-function RepFestivalList({ data = [], country }) {
+// import "./RepFestivalList.css";
+import "../css/RepFestivalList.css";
+function RepFestivalList({ data = [] }) {
   const today = new Date();
-  console.log(`today => ${today}`);
-  //const filterlocationArray = data.filter((item) => item.location === country);
+  // 오늘보다 축제 마지막 날짜가 큰 것으로 데이터 필터링
   const filterDateArray = data.filter((item) => new Date(item.date[1]) > today);
+  // 랜덤값 3개를 고름
   const getRandomIndexes = (max, count) => {
     const indexes = [];
     while (indexes.length < count) {
@@ -16,20 +17,13 @@ function RepFestivalList({ data = [], country }) {
     return indexes;
   };
   const randomIndexes = getRandomIndexes(filterDateArray.length, 3);
-
   return (
-    <table>
-      <tr>
-        {randomIndexes.map((index) => (
-          <td key={index}>
-            <RepFestival data={filterDateArray[index]} />
-          </td>
-        ))}
-      </tr>
-    </table>
+    <div className="repFestivalContainer">
+      {randomIndexes.map((index) => (
+        <RepFestival data={filterDateArray[index]} />
+      ))}
+    </div>
   );
 }
 
 export default RepFestivalList;
-//data에서 지역명이 같은 배열을 만들고 배열의 size-1까지 랜덤으로 돌려서 3개를 뽑음
-//그 3개를 각각의 컴포넌트에서 만들면 될 듯
