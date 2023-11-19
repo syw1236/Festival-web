7995357fe3167751672f90b5ea1562dab520197d
YeWon Seo <syw1236@naver.com>
2023-11-19 19:08:31 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./components/MainPage/Main";
 import CountryDetail from "./components/CountryDetail";
-import FestivalsData from "./data/FestivalsData";
+import FestivalDetail from "./components/FestivalDetail";
+import SearchList from "./components/SearchList";
+import TabPrint from "./components/TabPrint";
 function App() {
   return (
-    <div>
-      <CountryDetail data={FestivalsData} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/CountryDetail" element={<CountryDetail />} />
+        <Route path="/festival_detail/:id" element={<FestivalDetail />} />
+        <Route path="/searchList" element={<SearchList />} />
+        <Route path="/TabPrint" element={<TabPrint />} />
+      </Routes>
+    </Router>
   );
 }
 
