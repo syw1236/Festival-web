a93a0507d98a92092968cbcb472f04687ae5e4e8
YeWon Seo <syw1236@naver.com>
2023-11-19 19:08:15 +0900

    Update FistivalSimple.js

--- src/components/FistivalSimple.js
+++ src/components/FistivalSimple.js
@@ -1,16 +1,22 @@
-import "./FestivalSimple.css";
+// import "./FestivalSimple.css";
+import "../css/FestivalSimple.css";
+import { Link } from "react-router-dom";
 function FestivalSimple({ festival }) {
   return (
     <div className="festivalContain">
-      <img
-        className="festivalImage"
-        width={250}
-        height={250}
-        src={festival.poster}
-        alt="festival.poster"
-      />
-      <div className="festivalName">{festival.name}</div>
-      <div className="festivalLocation">{festival.detail_locations}</div>
+      <Link
+        to={`/festival_detail/${festival.id}`}
+        style={{ textDecoration: "none", color: "black" }}
+      >
+        <img
+          className="festivalImage"
+          src={festival.poster}
+          alt="festival.poster"
+        />
+
+        <div className="festivalName">{festival.name}</div>
+        <div className="festivalLocation">{festival.detail_locations}</div>
+      </Link>
     </div>
   );
 }
